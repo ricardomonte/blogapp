8979def3bf4c2e9e06680d8c32dc1ed2b183eb61
ricardo <ramn21@gmail.com>
2020-10-30 23:38:35 +0100

    DRY, create partial form,  methods in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
   end
   
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -13,11 +13,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_param)
     if @article.save
       flash[:notice] = 'Article was saved successfully'
       redirect_to article_path(@article)
@@ -27,8 +26,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_param)
       flash[:notice] = 'Article was updated successfully'
       redirect_to article_path(@article)
     else
@@ -37,8 +35,17 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_param
+    params.require(:article).permit(:title, :description)
   end
 end
