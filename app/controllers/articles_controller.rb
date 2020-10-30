90476b3ce6b930de0d289e2ec2f14b3bbb8c3663
ricardo <ramn21@gmail.com>
2020-10-30 21:31:50 +0100

    add helpers for display errors in new.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,17 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end 
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def display_error(article)
+    if article.errors.any?
+      content_tag(:h2, "Article coudnt be created")
+    end
+  end
+
+  def list_errors(article)
+    if article.errors.any?
+      content_tag(:ul) do
+        article.errors.full_messages.each do |msg|
+          concat content_tag(:li, msg)
+        end
+      end
+    end
+  end
+
 end
